dc527026518dcf4aa3d5d1475ebcb0fff7cf972d
dok1959 <dok1959.good@mail.ru>
2021-12-22 01:03:56 +1000

    Added role repository

---
+++ FreelanceWebServer/FreelanceWebServer/Repositories/UserRepository/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using FreelanceWebServer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreelanceWebServer.Repositories.UserRepository
+{
+    public interface IRoleRepository
+    {
+        public Task<IEnumerable<Role>> GetAll();
+
+        public Task<Role> Get(long id);
+    }
+}
---
+++ FreelanceWebServer/FreelanceWebServer/Repositories/UserRepository/PostgresRoleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using FreelanceWebServer.Data.PostgreSQL;
+using FreelanceWebServer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreelanceWebServer.Repositories.UserRepository
+{
+    public class PostgresRoleRepository : IRoleRepository
+    {
+        private readonly PostgresDBContext _context;
+
+        public PostgresRoleRepository(PostgresDBContext context) => _context = context;
+
+        public async Task<IEnumerable<Role>> GetAll()
+        {
+            var connection = await _context.GetFreeConnection();
+            return await connection.QueryAsync<Role>("SELECT * FROM \"roles\";");
+        }
+
+        public async Task<Role> Get(long id)
+        {
+            var connection = await _context.GetFreeConnection();
+            return await connection.QueryFirstOrDefaultAsync<Role>("SELECT * FROM \"roles\" WHERE id = @id;", new { id = id });
+        }
+    }
+}
