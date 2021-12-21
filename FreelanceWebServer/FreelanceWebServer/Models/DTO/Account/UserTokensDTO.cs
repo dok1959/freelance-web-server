6624d58eaa658c78164f12d8fb0735bf8de75b20
dok1959 <dok1959.good@mail.ru>
2021-12-22 01:02:31 +1000

    Removing old DTO versions

--- FreelanceWebServer/FreelanceWebServer/Models/DTO/Account/UserTokensDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FreelanceWebServer.Models.DTO.Account
-{
-    public class UserTokensDTO
-    {
-        public string AccessToken { get; set; }
-
-        public string RefreshToken { get; set; }
-    }
-}
