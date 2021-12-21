7dd8fccd308aac237ee00617ec1e563658bbbf8b
dok1959 <dok1959.good@mail.ru>
2021-12-22 01:05:29 +1000

    Added new DTOs for user

---
+++ FreelanceWebServer/FreelanceWebServer/DTO/Account/ProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FreelanceWebServer.Models.DTO.Account
+{
+    public class ProfileDTO
+    {
+        public string Username { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Name { get; set; }
+    }
+}
